a7eb5a7cc22edac2ebd26eec2db91781c1b0ae06
Michael Reinert <michael.reinert@wesolve-it.com>
2024-09-30 16:23:13 +0200

    changed some links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,10 +38,10 @@ export default function Footer() {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="#" className="hover:underline">Privacy Policy</a>
+                          <a href="https://flowbite.com/" className="hover:underline">Privacy Policy</a>
                       </li>
                       <li>
-                          <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                          <a href="https://flowbite.com/" className="hover:underline">Terms &amp; Conditions</a>
                       </li>
                   </ul>
               </div>
