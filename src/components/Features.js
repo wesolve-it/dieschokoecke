68fdbcfa4eabb5c633f66bfc78906b01ec8ba4fd
Michael Reinert <michael.reinert@wesolve-it.com>
2024-10-02 16:29:47 +0200

    changed some texts

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,25 +7,25 @@ import Truffel from '../assets/FeaturesTruffel.svg';
 export default function Features() {
   return (
     <div className='max-w-screen-xl px-6 xl:px-0 mx-auto mt-8 mb-12 xl:mt-12'>
-      <h2 className='text-2xl font-light mb-6 xl:text-5xl xl:mb-10 xl:w-2/5 xl:leading-snug'>Exceptional Features <span className='text-lg'>of Our</span> <span className='font-serif'>Chocolates</span></h2>
+      <h2 className='text-2xl font-light mb-6 xl:text-5xl xl:mb-10 xl:w-2/5 xl:leading-snug'>Außergewöhnliche Eigenschaften <span className='text-lg'>unserer</span> <span className='font-serif'>Schokolade</span></h2>
       <div className='xl:flex xl:flex-row xl:gap-x-12'>
         <section className='mb-8'>
           <section className='flex flex-row mb-4 xl:flex-col'>
-            <img src={Acorn} alt="An Acorn" className='h-10 xl:mr-auto' />
+            <img src={Acorn} alt="An Acorn" className='h-10 xl:mr-auto xl:mb-2' />
             <h3 className='my-auto ml-4 font-bold xl:ml-0'>Handcrafted Perfection</h3>
           </section>
           <p className='font-light'>Expertly crafted by skilled chocolatiers for a unique taste experience</p>
         </section>
         <section className='mb-8'>
           <section className='flex flex-row mb-4 xl:-mt-20 xl:flex-col'>
-            <img src={Coffee} alt="An Acorn" className='h-10 xl:mr-auto' />
+            <img src={Coffee} alt="An Acorn" className='h-10 xl:mr-auto xl:mb-2' />
             <h3 className='my-auto ml-4 font-bold xl:ml-0'>Rich Flavors</h3>
           </section>
           <p className='font-light'>A diverse range of flavors, from classic milk chocolate to intense dark varieties</p>
         </section>
         <section>
           <section className='flex flex-row mb-4 xl:-mt-20 xl:flex-col'>
-            <img src={Cake} alt="An Acorn" className='h-10 xl:mr-auto' />
+            <img src={Cake} alt="An Acorn" className='h-10 xl:mr-auto xl:mb-2' />
             <h3 className='my-auto ml-4 font-bold xl:ml-0'>Ethically Sourced Cocoa</h3>
           </section>
           <p className='font-light'>Made with the finest cocoa beans and high-quality natural ingredients</p>
--- src/components/Products.js
+++ src/components/Products.js
@@ -6,10 +6,10 @@ export default function Products() {
     <div className='max-w-screen-xl mx-auto px-6 xl:px-0 mt-12 xl:mt-0'>
       <div className='lg:mt-24 lg:flex-row-reverse lg:flex'>
         <div className='lg:flex-1 lg:my-auto'>
-          <h2 className='text-4xl text-center mb-6 lg:text-6xl lg:text-left font-extralight leading-snug'>Crafting Quality <span className='text-xl lg:text-3xl'>With</span> <span className='font-serif'>Every Detail</span></h2>
-          <p className='mb-6 leading-7 font-light'>We believe in the art of chocolate making. What started as a small family business has grown into a brand loved by chocolate enthusiasts worldwide. Our dedication to quality, sustainability, and innovation sets us apart. From bean to bar, we take pride in every step of the process.</p>
+          <h2 className='text-4xl text-center mb-6 lg:text-6xl lg:text-left font-extralight leading-snug'>Handwerkliche Qualität <span className='text-xl lg:text-3xl'>bis</span> <span className='font-serif'>ins Detail</span></h2>
+          <p className='mb-6 leading-7 font-light'>Belgische Kako-Trüffel unsere Gaumenschmeichler und Geheimtipp für Geburtstage, als Geschenke für gute Kunden, oder sich selbst zu beschenken. Verpackt im Geschenkkarton oder im Organzpäckchen.</p>
           <section className='text-center'>
-            <button className='rounded-full py-2 px-4 bg-chocolateBrown text-textBrown font-light uppercase'>Explore our chocolates</button>
+            <button className='rounded-full py-2 px-4 bg-chocolateBrown text-textBrown font-light uppercase'>Entdecke unsere Schokolade</button>
           </section>
         </div>
       <div className='lg:flex-1'>
