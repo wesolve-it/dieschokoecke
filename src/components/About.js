e0951d946d9824c5eb5fe90f3f418500cbfe49a4
Michael Reinert <michael.reinert@wesolve-it.com>
2024-10-02 16:44:23 +0200

    changed about text

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <div className='bg-bgBrown'>
       <div className='max-w-screen-xl mx-auto px-6 xl:px-0 pt-8 pb-8 xl:pt-14'>
-        <h2 className='text-center text-xl font-extralight lg:text-5xl lg:pr-72'>About</h2>
+        <h2 className='text-center text-xl font-extralight lg:text-5xl lg:pr-72'>Über</h2>
         <div className='lg:flex lg:flex-row'>
           <div className='hidden lg:block'>
             <img src={Image2} alt="Bild von Trüffeln" className='lg:h-2/3 lg:mt-12 lg:ml-[25%]' />
@@ -15,7 +15,7 @@ export default function About() {
           <div className='lg:flex-1'>
             <div>
               <h1 className='text-3xl text-center lg:text-left font-light mb-6 lg:text-5xl'>dieschokoecke</h1>
-              <p className='font-light text-left'>"Welcome to dieschokoecke, where passion meets craftsmanship. Our chocolates are made with the finest ingredients, ensuring every bite is a taste of luxury. Discover a world of flavor, from classic milk chocolate to adventurous dark varieties."</p>
+              <p className='font-light text-left'>„Willkommen bei dieschokoecke, wo Leidenschaft auf Handwerkskunst trifft. Unsere Schokoladen werden aus den besten Zutaten hergestellt, sodass jeder Bissen ein Hauch von Luxus ist. Entdecken Sie eine Welt voller Aromen, von klassischer Milchschokolade bis hin zu abenteuerlichen dunklen Sorten.“</p>
             </div>
             <div className='lg:flex lg:flex-row'>
               <img src={Image3} alt="Bild von Trüffeln" className='hidden lg:block lg:pl-20 lg:mt-32 lg:h-96' />
