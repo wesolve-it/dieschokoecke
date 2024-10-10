1c3f5e3bb726b2d553caab0f5d469aab2d88a2cb
Michael Reinert <michael.reinert@wesolve-it.com>
2024-10-10 16:35:28 +0200

    fixed things

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Florentiner from '../assets/Florentiner.jpeg';
 
 export default function Product({product}) {
   const { price, name, image, description } = product;
 
   return (
     <div class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-      <img class="h-96 w-full object-cover object-center" src={image} alt="Product Image" />
+      <img class="h-96 w-full object-cover object-center" src={image} alt={name} />
       <div class="p-4">
         <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>
         <p class="mb-2 text-base dark:text-gray-300 text-gray-700">{description}</p>
