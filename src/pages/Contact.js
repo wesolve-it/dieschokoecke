611a43f3ff26f5ab42bdca8101b6ba53dda7404f
Michael Reinert <michael.reinert@wesolve-it.com>
2024-10-11 15:55:48 +0200

    changed name of hidden field

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,7 +21,7 @@ export default function Contact() {
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Kontaktiere uns</h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Haben Sie ein technisches Problem? Möchten Sie Feedback zu einer Beta-Funktion senden? Benötigen Sie Details zu unserem Businessplan? Lassen Sie es uns wissen.</p>
       <form name="contact" method='POST' data-netlify="true" className="space-y-8" onSubmit={handleSubmit}>
-        <input type='hidden' name="contact" value="contact" />
+        <input type='hidden' name="form-name" value="contact" />
           <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
               <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@adresse.com" required />
