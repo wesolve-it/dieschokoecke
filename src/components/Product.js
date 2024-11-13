884f1494ae3ad8494744ccb4e3b1c1877fb11b20
Michael Reinert <michael.reinert@wesolve-it.com>
2024-11-13 11:45:46 +0100

    deleted € icon from product

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,7 +12,7 @@ export default function Product({product}) {
         <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{title}</h2>
         <p className="mb-2 text-base dark:text-gray-300 text-gray-700">{description}</p>
         <div className="flex items-center">
-          <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{price}€</p>
+          <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{price}</p>
           {/* <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p> */}
           {/* <p className="ml-auto text-base font-medium text-green-500">20% off</p> */}
         </div>
