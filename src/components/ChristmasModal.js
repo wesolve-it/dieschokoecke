09d69bc1315952d11c9f6388ca0f4970aff77136
Michael Reinert <michael.reinert@wesolve-it.com>
2024-12-07 16:18:53 +0100

    changed christmas text

--- src/components/ChristmasModal.js
+++ src/components/ChristmasModal.js
@@ -19,7 +19,7 @@ const ChristmasModal = () => {
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-red-600 text-white p-8 rounded-lg w-80 text-center shadow-lg">
         <h2 className="text-2xl font-bold mb-4">Frohe Weihnachten!</h2>
-        <p className="mb-4">In der Weihnachtszeit können Sie samstags von 9 bis 13 Uhr bei uns bestellen.</p>
+        <p className="mb-4">In der Weihnachtszeit können Sie samstags von 9 bis 13 Uhr bei uns bestellen und ihre Lieferung abholen..</p>
         <div className="text-xl mb-4">🎄</div>
         <button
           className="mt-4 px-6 py-2 bg-green-500 hover:bg-green-600 rounded-full transition"
