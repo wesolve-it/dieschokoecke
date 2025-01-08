19200aa9422a59fcc40abc58930f06a0de7ab823
Michael Reinert <michael.reinert@wesolve-it.com>
2025-01-08 14:00:31 +0100

    deleted unnessecary import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import About from '../components/About';
 import Features from '../components/Features';
 import Reviews from '../components/Reviews';
 import FAQ from '../components/FAQ';
-import ChristmasModal from '../components/ChristmasModal';
 
 export default function Home({faqEntries}) {
   return (
